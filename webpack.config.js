8261777f0f1147b62d34ea9e89d895fc5a17e0e2
lloooo <409023726@qq.com>
2018-05-10 00:22:51 +0800

    css

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,24 @@ module.exports = {
           loader : "babel-loader",
         },
         exclude : /node_modules/
+      },
+      {
+        test : /\.css$/,
+        use : [
+          {
+            loader : "style-loader",
+          },
+          {
+            loader : "css-loader",
+            options : {
+              modules : true,
+              localIdentName : '[name]__[local]--[hash:base64:5]'
+            }
+          },
+          {
+            loader : "postcss-loader"
+          }
+        ]
       }
     ]
   }
