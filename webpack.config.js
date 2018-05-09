5e47ee1816037799a8faa8f1b9c8db1fde59c43d
lloooo <409023726@qq.com>
2018-05-09 16:31:55 +0800

    ok

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry : __dirname + "/app/main.js",
+  output : {
+    path : __dirname + "/public",
+    filename : "bundle.js",
+  },
+  devtool : "eval-source-map",
+
+  devServer : {
+    contentBase : "./public",
+    historyApiFallback : true,
+    inline : true,
+  }
+};
